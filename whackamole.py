796de6edf7f769b2125939da52fd7a826990c6b3
wyotwastaken <wyatt.scheinbaum@ufl.edu>
2024-11-17 21:38:16 -0500

    Summary of changes
    Changed display code to be a for loop rather than a bunch of line print statements. Fixed the logic regarding the placement of the mole, recognizing if it's grid has been clicked, and generating a new set of coordinates to place it.

--- whackamole.py
+++ whackamole.py
@@ -1,59 +1,41 @@
 import pygame
 import random
 
+screen = pygame.display.set_mode((640, 512))
+mole_image = pygame.image.load("mole.png")
 
 def main():
+    #variable initialization
+    x_rows = 20
+    y_rows = 16
+    box_dim = 32
+    mole_row = 0
+    mole_col = 0
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        screen = pygame.display.set_mode((640, 512))
-        mole_image = pygame.image.load("mole.png")
-        mole_initial_position = [0, 0]
         clock = pygame.time.Clock()
         running = True
         while running:
+            #filling board
+            screen.fill("light green")
+            for i in range(1, x_rows):
+                pygame.draw.line(screen, (0, 0, 0), (i * box_dim, 0), (i * box_dim, 512))
+            for j in range(1, y_rows):
+                pygame.draw.line(screen, (0, 0, 0), (0, j * box_dim), (640, j * box_dim))
+            #mole display
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_row*box_dim, mole_col*box_dim)))
+            pygame.display.flip()
+            #event conditions
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
-            pygame.draw.line(screen, (0, 0, 0), (32, 0), (32, 640))
-            pygame.draw.line(screen, (0, 0, 0), (64, 0), (64, 640))
-            pygame.draw.line(screen, (0, 0, 0), (96, 0), (96, 640))
-            pygame.draw.line(screen, (0, 0, 0), (128, 0), (128, 640))
-            pygame.draw.line(screen, (0, 0, 0), (160, 0), (160, 640))
-            pygame.draw.line(screen, (0, 0, 0), (192, 0), (192, 640))
-            pygame.draw.line(screen, (0, 0, 0), (224, 0), (224, 640))
-            pygame.draw.line(screen, (0, 0, 0), (256, 0), (256, 640))
-            pygame.draw.line(screen, (0, 0, 0), (288, 0), (288, 640))
-            pygame.draw.line(screen, (0, 0, 0), (320, 0), (320, 640))
-            pygame.draw.line(screen, (0, 0, 0), (352, 0), (352, 640))
-            pygame.draw.line(screen, (0, 0, 0), (384, 0), (384, 640))
-            pygame.draw.line(screen, (0, 0, 0), (416, 0), (416, 640))
-            pygame.draw.line(screen, (0, 0, 0), (448, 0), (448, 640))
-            pygame.draw.line(screen, (0, 0, 0), (480, 0), (480, 640))
-            pygame.draw.line(screen, (0, 0, 0), (512, 0), (512, 640))
-            pygame.draw.line(screen, (0, 0, 0), (544, 0), (544, 640))
-            pygame.draw.line(screen, (0, 0, 0), (576, 0), (576, 640))
-            pygame.draw.line(screen, (0, 0, 0), (608, 0), (608, 640))
-            pygame.draw.line(screen, (0, 0, 0), (0, 32), (640, 32))
-            pygame.draw.line(screen, (0, 0, 0), (0, 64), (640, 64))
-            pygame.draw.line(screen, (0, 0, 0), (0, 96), (640, 96))
-            pygame.draw.line(screen, (0, 0, 0), (0, 128), (640, 128))
-            pygame.draw.line(screen, (0, 0, 0), (0, 160), (640, 160))
-            pygame.draw.line(screen, (0, 0, 0), (0, 192), (640, 192))
-            pygame.draw.line(screen, (0, 0, 0), (0, 224), (640, 224))
-            pygame.draw.line(screen, (0, 0, 0), (0, 256), (640, 256))
-            pygame.draw.line(screen, (0, 0, 0), (0, 288), (640, 288))
-            pygame.draw.line(screen, (0, 0, 0), (0, 320), (640, 320))
-            pygame.draw.line(screen, (0, 0, 0), (0, 352), (640, 352))
-            pygame.draw.line(screen, (0, 0, 0), (0, 384), (640, 384))
-            pygame.draw.line(screen, (0, 0, 0), (0, 416), (640, 416))
-            pygame.draw.line(screen, (0, 0, 0), (0, 448), (640, 448))
-            pygame.draw.line(screen, (0, 0, 0), (0, 480), (640, 480))
-            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
-            if pygame.MOUSEBUTTONDOWN:
-                pass
-            pygame.display.flip()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    row = x // box_dim
+                    col = y // box_dim
+                    if row == mole_row and col == mole_col:
+                        mole_row = random.randrange(0, x_rows)
+                        mole_col = random.randrange(0, y_rows)
             clock.tick(60)
     finally:
         pygame.quit()
